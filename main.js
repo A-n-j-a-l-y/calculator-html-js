d38b871328b6b00c5b2e74ca8e810c22ec810b41
A-n-j-a-l-y <anjalyksomarajan2@gmai.com>
2023-11-21 16:53:31 +0530

    calculator

---
+++ main.js
@@ -0,0 +1,23 @@
+
+const displayData = (data)=>{
+    result.value += data
+}
+
+const calclear = () =>{
+    result.value = ''
+}
+
+const calresult = ()=>{
+    if(result.value!=''){
+        try{
+            result.value = eval(result.value)
+        }
+        catch{
+            result.value = 'Error ! ! !'
+        }
+    }
+}
+
+const removeLast = ()=>{
+    result.value = result.value.slice(0,-1)
+}
